6e14d837edf8363b06d44c943741d34af436eb11
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-11-03 21:03:44 -0600

    recovering the few changes I lost.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="row fluid mt-4" style={{ backgroundColor: "red" }}>
+        <div className="row fluid mt-4" style={{backgroundColor: "red", fontFamily: "'Luckiest Guy', cursive"}}>
             <div className="col-md-12">
                 <br />
                 <br />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ function Navbar(props) {
     return (
             <div className="row sticky-top d-flex justify-content-center text-center poketext" style={{backgroundColor: "red", fontFamily: "'Luckiest Guy', cursive"}}>
                 <div className="col-md-4">
-                    <h4 className="font-weight-bold">Clicky Game</h4>
+                    <h3 className="font-weight-bold">Clicky Game</h3>
                 </div>
                 <div className="col-md-4">
-                    <h4>Click an image to begin!</h4>
+                    <h3>Click an image to begin!</h3>
                 </div>
                 <div className="col-md-4">
-                    <h4>Score: {props.score} | Top Score: {props.topScore}</h4>
+                    <h3>Score: {props.score} | Top Score: {props.topScore}</h3>
                 </div>
              </div>
 
